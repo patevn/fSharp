a438867ea835dbc08b7241c3fab4f58352080235
pete mc <petermmcnaught@gmail.com>
2022-05-16 14:38:13 +1000

    101

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+
+let sayHello person =
+    printfn "Hello %s from my F# program!" person
+
+[<EntryPoint>]
+let main argv =
+    Array.iter sayHello argv
+    let peter: string = "HI"
+    printfn "Nice to meet you."
+    0
