4ebb560e898658e305eb2544d8dab383dfd6f38d
pete mc <petermmcnaught@gmail.com>
2022-05-17 10:34:15 +1000

    Collection Functions

--- Program.fs
+++ Program.fs
@@ -1,11 +1,36 @@
 ﻿open System
+open System.IO
 
-let sayHello person =
-    printfn "Hello %s from my F# program!" person
+let printMeanScore (row : string) =
+    let elements = row.Split('\t')
+    let name = elements.[0]
+    let id = elements.[1]
+    let students = elements |> Array.skip 2 |> Array.map float
+
+    let meanScore = students |> Array.average
+    let highScore = students |> Array.max 
+    let lowScore = students|>  Array.min 
+    printfn "%s\t%s\t%0.1f\t%0.1f\t%0.1f" name id meanScore highScore lowScore
+
+let summarize filePath=
+    let rows = File.ReadAllLines filePath
+    let studentCount= (rows |> Array.length) - 1
+    printfn "The number of students is %i " studentCount
+    printfn "name\t\tid\tmeanScore\thighScore\tlowScore"  
+    rows |> Array.skip 1 |> Array.iter printMeanScore
+
 
 [<EntryPoint>]
 let main argv =
-    Array.iter sayHello argv
-    let peter: string = "HI"
-    printfn "Nice to meet you."
-    0
+    if argv.Length = 1 then
+        let filePath = argv.[0]
+        if File.Exists filePath then
+            printfn "Processing %s " filePath
+            summarize filePath
+            0
+        else
+           printfn "File not found: %s " filePath  
+           2
+    else
+        printfn "you need a file bro"
+        1  // this is just a exit  code as a return statement
