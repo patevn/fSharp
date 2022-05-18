2f6a3f4d666f6962886a3a7e2c425489c18447cd
pete mc <petermmcnaught@gmail.com>
2022-05-18 15:44:36 +1000

    Record Types

--- Program.fs
+++ Program.fs
@@ -1,23 +1,45 @@
 ﻿open System
 open System.IO
 
-let printMeanScore (row : string) =
-    let elements = row.Split('\t')
-    let name = elements.[0]
-    let id = elements.[1]
-    let students = elements |> Array.skip 2 |> Array.map float
+type Student = 
+    {
+    Name : string
+    Id : string
+    MeanScore : float
+    MinScore : float
+    MaxScore : float
 
-    let meanScore = students |> Array.average
-    let highScore = students |> Array.max 
-    let lowScore = students|>  Array.min 
-    printfn "%s\t%s\t%0.1f\t%0.1f\t%0.1f" name id meanScore highScore lowScore
+    }
+
+module Student =   // module is just a group fo code, like in JS
+    let fromString(s: string) = 
+        let elements = s.Split('\t')
+        let name = elements.[0]
+        let id = elements.[1]
+        let students = elements |> Array.skip 2 |> Array.map float
+
+        let meanScore = students |> Array.average
+        let maxScore = students |> Array.max 
+        let minScore = students|>  Array.min
+        { // the last thing in a function is the return
+            Name = name
+            Id = id
+            MeanScore = meanScore
+            MinScore = minScore
+            MaxScore = maxScore
+        }
+
+
+    let printSummary (s : Student) =
+        printfn "%s\t%s\t%0.1f\t%0.1f\t%0.1f" s.Name s.Id s.MeanScore s.MaxScore s.MinScore
 
 let summarize filePath=
-    let rows = File.ReadAllLines filePath
-    let studentCount= (rows |> Array.length) - 1
+    let rows: Student[] = File.ReadAllLines filePath  |> Array.skip 1 |> Array.map Student.fromString |> Array.sortBy(fun student -> student.Name)  // fun here is how F# does lambda (anons)
+
+    let studentCount= (rows |> Array.length) - 1 
     printfn "The number of students is %i " studentCount
     printfn "name\t\tid\tmeanScore\thighScore\tlowScore"  
-    rows |> Array.skip 1 |> Array.iter printMeanScore
+    rows |> Array.skip 1 |> Array.iter Student.printSummary 
 
 
 [<EntryPoint>]
